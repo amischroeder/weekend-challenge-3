e5c87f541bad793b61a630427904a9a9bfa03722
Ami Schroeder <arschroeder22@gmail.com>
2017-08-12 09:52:54 -0500

    set up client side GETand POST requests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,44 @@
 $(document).ready(function () {
     console.log('Jquery loaded');
+    getTasks();
+
+    $('#addButton').on('click', function(){
+        console.log('Add button clicked!');
+        var taskIn = $('#taskIn').val();
+        var inputObject = {
+            task: taskIn
+        }
+    });
 }); //end doc ready
+
+function postTask(newTask) {
+  console.log('in postTask', newTask);
+  $.ajax({
+    url: '/tasks',
+    type: 'POST',
+    data: newTask,
+    success: function (data) {
+      console.log('got some tasks: ', data);
+      getTasks(data);
+    } 
+  }); 
+}
+
+function getTasks() {
+  console.log('in getTasks');
+  $.ajax({
+    url: '/tasks',
+    type: 'GET',
+    success: function (data) {
+      console.log('got some tasks: ', data);
+      displayTasks(data);
+    } 
+  }); 
+}
+
+function displayTasks(tasksArray) {
+    $('#taskList').empty();
+    for(var i=0; i<tasksArray.length; i++) {
+        var task = tasksArray[i];
+    }
+}
