34651595b99300f31244839f31ae0c1dc8ba2de5
Ami Schroeder <arschroeder22@gmail.com>
2017-08-12 10:49:39 -0500

    POST is working now

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -21,4 +21,27 @@ router.get('/', function(req,res){
     });
 });
 
+router.post('/', function(req, res){
+	console.log('task post was hit!');
+	pool.connect(function(errorConnectingToDatabase, client, done){
+		if(errorConnectingToDatabase) {
+			// when connecting to database failed
+			console.log('Error connecting to database', errorConnectingToDatabase);
+			res.sendStatus(500);
+		} else {
+			// when connecting to database worked!
+            client.query('INSERT INTO tasks (task) VALUES ($1);', 
+            [req.body.task], function(errorMakingQuery, result) {
+				done();
+				if(errorMakingQuery) {
+					console.log('Error making database query', errorMakingQuery);
+					res.sendStatus(500);
+				} else {
+					res.sendStatus(201);
+				}
+			});
+		}
+	});
+});
+
 module.exports = router;
